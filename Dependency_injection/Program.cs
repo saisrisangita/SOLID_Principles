14eb0399068a1067c7bf0e994f046566dbe0e6c0
saisrisagita <your_email@example.com>
2023-07-18 10:33:11 +0530

    initial commit

---
+++ Dependency_injection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public interface ITemperatureSensor
+{
+    double GetTemperature();
+}
+
+public class WeatherTracker
+{
+    private ITemperatureSensor temperatureSensor;
+
+    public WeatherTracker(ITemperatureSensor sensor)
+    {
+        temperatureSensor = sensor;
+    }
+
+    public void DisplayTemperature()
+    {
+        double temperature = temperatureSensor.GetTemperature();
+        Console.WriteLine("Current temperature: " + temperature);
+    }
+}
+
+public class Thermometer : ITemperatureSensor
+{
+    public double GetTemperature()
+    {
+       
+        return 25.0;
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        ITemperatureSensor thermometer = new Thermometer();
+        WeatherTracker weatherTracker = new WeatherTracker(thermometer);
+
+        weatherTracker.DisplayTemperature();
+
+        Console.ReadLine();
+    }
+}
---
+++ interface_segregation/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public interface IWorker
+{
+    void Work();
+}
+
+public interface IEater
+{
+    void Eat();
+}
+
+public interface ISleeper
+{
+    void Sleep();
+}
+
+public class Robot : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot working...");
+    }
+}
+
+public class Human : IWorker, IEater, ISleeper
+{
+    public void Work()
+    {
+        Console.WriteLine("Human working...");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Human eating...");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Human sleeping...");
+    }
+}
+
+public class Animal : IEater, ISleeper
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal eating...");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal sleeping...");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IWorker worker = new Human();
+        worker.Work();
+
+        IEater eater = new Human();
+        eater.Eat();
+
+        ISleeper sleeper = new Human();
+        sleeper.Sleep();
+
+        eater = new Animal();
+        eater.Eat();
+
+        sleeper = new Animal();
+        sleeper.Sleep();
+    }
+}
